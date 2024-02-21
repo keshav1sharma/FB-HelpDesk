c87d992c36679588bd7e0caa9d53323b109e1f05
keshav1sharma <keshavsharma8000@gmail.com>
2024-02-22 00:22:01 +0530

    Add webhook endpoint to handle page webhooks

--- server/index.js
+++ server/index.js
@@ -25,11 +25,31 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
 app.use('/auth', authRoute);
 app.use('/fb', authRoute);
 
+// Webhook
+app.post("/webhook", (req, res) => {
+    let body = req.body;
+
+    console.log(`\u{1F7EA} Received webhook:`);
+    console.dir(body, { depth: null });
+
+      // Send a 200 OK response if this is a page webhook
+
+  if (body.object === "page") {
+    // Returns a '200 OK' response to all requests
+    res.status(200).send("EVENT_RECEIVED");
+    // Determine which webhooks were triggered and get sender PSIDs and locale, message content and more.
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+}); 
 
 app.get('/', (req, res) => {
     res.send('Server is running!');
     }
 );
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
