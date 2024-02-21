364c5108d0b6c3ca41d5af7b49fef6e8b1f4cb32
keshav1sharma <keshavsharma8000@gmail.com>
2024-02-22 00:23:10 +0530

    Fix webhook handling and add messaging webhook verification

--- server/index.js
+++ server/index.js
@@ -15,10 +15,10 @@ app.use(express.urlencoded({ extended: true }));
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log('Connected to MongoDB');
-    }
+}
 ).catch((error) => {
     console.log('Error:', error);
-    }
+}
 );
 
 // Routes
@@ -32,26 +32,47 @@ app.post("/webhook", (req, res) => {
     console.log(`\u{1F7EA} Received webhook:`);
     console.dir(body, { depth: null });
 
-      // Send a 200 OK response if this is a page webhook
+    // Send a 200 OK response if this is a page webhook
 
-  if (body.object === "page") {
-    // Returns a '200 OK' response to all requests
-    res.status(200).send("EVENT_RECEIVED");
-    // Determine which webhooks were triggered and get sender PSIDs and locale, message content and more.
-  } else {
-    // Return a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-}); 
+    if (body.object === "page") {
+        // Returns a '200 OK' response to all requests
+        res.status(200).send("EVENT_RECEIVED");
+        // Determine which webhooks were triggered and get sender PSIDs and locale, message content and more.
+    } else {
+        // Return a '404 Not Found' if event is not from a page subscription
+        res.sendStatus(404);
+    }
+});
+
+app.get("/messaging-webhook", (req, res) => {
+
+    // Parse the query params
+    let mode = req.query["hub.mode"];
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
+
+    // Check if a token and mode is in the query string of the request
+    if (mode && token) {
+        // Check the mode and token sent is correct
+        if (mode === "subscribe" && token === config.verifyToken) {
+            // Respond with the challenge token from the request
+            console.log("WEBHOOK_VERIFIED");
+            res.status(200).send(challenge);
+        } else {
+            // Respond with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
+    }
+});
 
 app.get('/', (req, res) => {
     res.send('Server is running!');
-    }
+}
 );
 
 
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-    }
+}
 );
